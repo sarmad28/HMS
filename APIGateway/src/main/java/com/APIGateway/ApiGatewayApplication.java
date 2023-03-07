a2d9e72ee4fd2811370c84e5371a81b50df350b3
sarmad shaikh <shaikhsarmad28@gmail.com>
2023-03-07 18:03:36 +0500

    added feignClients and apigateway

--- APIGateway/src/main/java/com/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/APIGateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package com.UserService;
+package com.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-public class UserServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
